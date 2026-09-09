8fb75c06bf8508d8dd22367108aaf61023e028fa
Paiva <danpaiva@live.com>
2026-09-09 13:38:01 -0300

    refact: refatorando codigo

--- ConsumerCatFactApi/Models/CatFact.cs
+++ ConsumerCatFactApi/Models/CatFact.cs
@@ -2,7 +2,6 @@ using System.Text.Json.Serialization;
 
 namespace ConsumerCatFactApi.Models;
 
-// Representação do JSON retornado pela API
 public record CatFact(
     [property: JsonPropertyName("fact")] string Fact,
     [property: JsonPropertyName("length")] int Length
--- ConsumerCatFactApi/Program.cs
+++ ConsumerCatFactApi/Program.cs
@@ -1,24 +1,21 @@
 ﻿using System.Net.Http.Json;
-using System.Text.Json.Serialization;
-using ConsumerCatFactApi.Context;
+using ConsumerCatFactApi.Models;
 
 using var client = new HttpClient();
 
 try
 {
     Console.WriteLine("\nBuscando um fato sobre gatos...\n");
-    var catFact = await client.GetFromJsonAsync("https://catfact.ninja/fact", CatFactContext.Default.CatFact);
+
+    var catFact = await client.GetFromJsonAsync<CatFact>("https://catfact.ninja/fact");
 
     if (catFact is not null)
     {
-        Console.WriteLine($"\nFact: {catFact.Fact}");
-        Console.WriteLine($"\nLength: {catFact.Length}");
+        Console.WriteLine($"Fact: {catFact.Fact}");
+        Console.WriteLine($"Length: {catFact.Length}");
     }
 }
 catch (Exception ex)
 {
     Console.WriteLine($"\nErro ao consultar a API: {ex.Message}");
 }
-
-
-
